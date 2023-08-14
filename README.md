7bcfc1d816c023f59292d79645f79f1761147840
mitchell-devpipline <122054442+mitchell-devpipline@users.noreply.github.com>
2023-08-14 15:04:39 -0600

    added auth controller

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # BEcapstone
+
+Welcome to Mitchell Nelsons BE Capstone.
+I designed this BE application for an online grocery store that only sells whole foods.
+
+Spin up the app using python app.py
---
+++ controllers/auth_controller.py
@@ -0,0 +1,35 @@
+from flask import jsonify, request
+from flask_bcrypt import check_password_hash
+
+from datetime import datetime, timedelta
+
+from models.users import Users
+from models.auth_tokens import AuthTokens, auth_token_schema
+from db import db
+
+
+def auth_token_add():
+    token_request = request.json
+    email = token_request.get('email')
+    password = token_request.get("password")
+    expiry = datetime.now() + timedelta(hours=12)
+    user_data = db.session.query(Users).filter(Users.email == email).filter(Users.active).first()
+
+    if not email or not password or not user_data:
+        return jsonify({"message": "Invalid Login"}), 401
+
+    valid_password = check_password_hash(user_data.password, password)
+
+    if not valid_password:
+        return jsonify({"message": "Invalid Login"}), 401
+
+    existing_token = db.session.query(AuthTokens).filter(AuthTokens.user_id == user_data.user_id).all()
+
+    if existing_token:
+        for token in existing_token:
+            if token.expiration < datetime.now():
+                db.session.delete(token)
+    new_token = AuthTokens(user_data.user_id, expiry)
+    db.session.add(new_token)
+    db.session.commit()
+    return jsonify({'message': {"auth_token": auth_token_schema.dump(new_token)}})
